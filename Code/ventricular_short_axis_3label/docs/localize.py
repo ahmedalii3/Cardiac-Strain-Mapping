4618c01a3c5550f9423cd0a54924395a6473f8db
Ahmed Mohamed Ali <ahmed.rajab502@eng-st.cu.edu.eg>
2024-12-06 15:34:43 +0200

    Localize model

---
+++ Code/ventricular_short_axis_3label/docs/localize.py
@@ -0,0 +1,157 @@
+import matplotlib.pyplot as plt
+import monai
+import torch
+import numpy as np
+import SimpleITK as sitk
+import scipy.interpolate as spi
+from scipy.ndimage import zoom
+import os
+from scipy.ndimage import center_of_mass
+
+class Localize:
+    def __init__(self, time_series):
+        self.image = None
+        self.seg = None
+        self.center_mass = None
+        self.median = None  
+        self.array_of_cropped_images = []
+        self.array_of_images_in_time_series = {}
+        self.time_series = time_series
+        self.images_with_center_mass = []
+        self.center_of_masses_dictionary = {}
+        self.calculate_median(self.time_series)
+        self.localize_image()
+        
+
+    def get_cropped_image(self, image_number):
+        return self.array_of_cropped_images[image_number]
+    
+    def view_center_of_mass(self, image_number):
+
+        i = 0
+        for key, value in self.center_of_masses_dictionary.items():
+            if i == image_number:
+               
+                plt.imshow(self.array_of_images_in_time_series[key], cmap='gray')
+                plt.scatter(value[0], value[1], c='red')
+                plt.show()
+                break
+            i += 1
+
+    def localize_image(self):
+        self.array_of_cropped_images = []
+        for root, dirs, files in os.walk(self.time_series):
+            for file in files:
+                if file.endswith(".npy"):
+                    im = np.load(os.path.join(root, file))
+                    im = im[0,:,:]
+                    self.array_of_images_in_time_series[file] = im
+                    center = self.center_of_masses_dictionary[file]
+                    if np.linalg.norm(center - self.median) > 30:
+                        center = self.median
+                        self.center_of_masses_dictionary[file] = center
+                    cropped_image = self.crop_image(im, center)
+                    self.array_of_cropped_images.append(cropped_image)
+
+    def calculate_median(self, time_series):
+        center_mass = []
+        
+        for root, dirs, files in os.walk(time_series):
+            for file in files:
+                if file.endswith(".npy"):
+                    im = np.load(os.path.join(root, file))
+                    mask = im[1,:,:]
+                    im = im[0,:,:]
+                    self.image = im
+                    seg = self.predict_seg()
+                    center = self.calculate_center_of_mass(seg)
+                    center_mass.append(center)
+                    self.center_of_masses_dictionary[file] = center
+        
+        self.median = np.median(center_mass, axis=0)
+        
+                     
+    def remove_outliers_zscore(self, mask, target_value, threshold = 0.5):
+        x_distribution = np.sum(mask == target_value, axis=0)  # Sum target values along rows
+        x_mean = np.mean(x_distribution)
+        x_std = np.std(x_distribution)
+        x_zscores = (x_distribution - x_mean) / x_std
+        x_outliers = (x_zscores < threshold)  # Identify outlier columns
+        mask[:, x_outliers] = np.where(mask[:, x_outliers] == target_value, 0, mask[:, x_outliers])
+
+        # Y distribution (rows)
+        y_distribution = np.sum(mask == target_value, axis=1)  # Sum target values along columns
+        y_mean = np.mean(y_distribution)
+        y_std = np.std(y_distribution)
+        y_zscores = (y_distribution - y_mean) / x_std
+        y_outliers = (y_zscores < threshold)  # Identify outlier rows
+        mask[y_outliers, :] = np.where(mask[y_outliers, :] == target_value, 0, mask[y_outliers, :])
+
+        return mask
+            
+    def predict_seg(self):
+        # bundle_path = "/Users/ahmed_ali/Downloads/ventricular_short_axis_3label"
+        bundle_path = os.path.join(os.path.dirname(__file__), "..")
+        model_path = os.path.join(bundle_path, "models", "model.pt")
+
+        parser = monai.bundle.load_bundle_config(bundle_path, "train.json")
+        # parser = monai.bundle.load_bundle_config("..", "train.json")
+        net = parser.get_parsed_content("network_def")  # "network" loads the network into GPU which we'll avoid for simplicity here
+        # net.load_state_dict(torch.load("../models/model.pt"))
+        # net.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+        net.load_state_dict(torch.load(model_path, map_location=torch.device('cpu'), weights_only=True))
+        inputd = torch.from_numpy(self.image / self.image.max()).float()
+        # inputd = torch.from_numpy(im).float()
+        pred = net(inputd[None, None, :, :])  # adding batch and channel dimensions to inputd
+        pred = torch.softmax(pred[0], dim=0)  # activation and remove batch dimension
+        self.seg = torch.argmax(pred, dim=0).data.numpy()  # convert to segmentation
+        self.process_seg()
+        return self.seg
+
+    def process_seg(self):
+        self.seg[self.seg == 3] = 0   # remove the mask of value 3
+        self.seg = self.remove_outliers_zscore(self.seg, 1)
+        self.seg = self.remove_outliers_zscore(self.seg, 2)
+
+    def calculate_center_of_mass(self, seg):
+        self.center_mass = center_of_mass(seg)
+        return self.center_mass
+    
+    def crop_image(self, image, center_mass, size = 128):
+        cropx, cropy = size, size   # size of the cropped image
+        x, y = center_mass
+        startx = int(x - cropx // 2)
+        starty = int(y - cropy // 2)
+        return image[starty:starty + cropy, startx:startx + cropx]
+
+    
+    def plot_image_seg(self):
+        plt.imshow(self.image, cmap='gray')
+        plt.imshow(self.seg, cmap='jet',alpha=0.5)
+        plt.show()
+
+    def plot_image(self , image):
+        plt.imshow(image, cmap='gray')
+        plt.show()  
+
+
+
+    
+
+# sample usage
+
+# im = np.load("/Users/ahmed_ali/Documents/GitHub/GP-2025-Strain/Data/ACDC/database/train_numpy/patient085/patient085_frame01_slice_10_ACDC.npy")
+# mask = im[1,:,:]
+# im = im[0,:,:]
+
+Localizer = Localize("/Users/ahmed_ali/Documents/GitHub/GP-2025-Strain/Data/ACDC/database/train_numpy/patient085")  # path to the time series folder
+image = Localizer.get_cropped_image(5)
+Localizer.plot_image(image)
+Localizer.view_center_of_mass(5)
+
+
+        
+    
+
+    
+            
